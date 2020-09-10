51d9f7afdb64bc73ee4d566eab6ec0f1f88e9ca6
prasanth <prashanthmsktm@gmail.com>
2020-09-10 15:12:29 +0530

    jenkinse upae

--- src/App.js
+++ src/App.js
@@ -27,14 +27,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
